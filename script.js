b224c4a6f59db4df7eeced31ae5e2a9a63843e1b
FernandoZib <60972217+FernandoZib@users.noreply.github.com>
2025-06-27 13:36:43 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("seal").addEventListener("click", function () {
+  document.getElementById("wrapper").classList.add("open");
+  document.getElementById("seal").classList.add("hide-seal");
+});
